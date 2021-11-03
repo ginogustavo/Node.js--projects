ca284bb484f8e48e6451140e84ba7db894f824b8
Gino Ureta <ing.gino@gmail.com>
2021-11-03 04:36:26 -0400

    Installing 3rd party Libraries

--- workflow-and-debugging/app.js
+++ workflow-and-debugging/app.js
@@ -14,3 +14,25 @@ server.listen(3000);
  * But if we add another e.g. "start-server" it won't work as "npm start-server"
  * The new ones need to work with "npm run start-server"
  */
+
+/** Dependencies can be developement (e.g. nodemon)
+ * or production packages(where we dont need there)
+ *
+ * install [package] --save (it'd be installed as a production dependency)
+ * install [package] --save-dev (used only during development)
+ *
+ * -g (will install not only for the project but for your machine, so you
+ * can use it anywhere)
+ *
+ * Install: npm install nodemon --save-dev
+ * - it creates the node_modules directory
+ * - it also creates the package-lock.json
+ *      stores exact version I installed(instead of the latest version)
+ * - updated package.json file (new devDependency section)
+ *    ^ indicates how the package will be updated if you re-run $npm install
+ *      without defining an extra package name, bc this command alone will go
+ *      through all you packages mentioned in package.json and install them and
+ *      automatically pick a later version if avaialble.
+ *
+ *
+ * */
